5bad43d18503ef81cdba14df5f23ab89ca9f8d79
Jani <janij.timonen@gmail.com>
2016-02-20 12:40:56 +0200

    New temp ui is implemented

--- app/src/main/java/tamk/tiko/com/weatherapp/MainActivity.java
+++ app/src/main/java/tamk/tiko/com/weatherapp/MainActivity.java
@@ -2,19 +2,14 @@ package tamk.tiko.com.weatherapp;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-
-import org.json.JSONObject;
 
 import java.util.concurrent.ExecutionException;
 
@@ -24,14 +19,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ImageView refresh = (ImageView) findViewById(R.id.refreshButton);
+        refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView text = (TextView)findViewById(R.id.testString);
+                TextView text = (TextView)findViewById(R.id.tempertureText);
                 TextView city = (TextView)findViewById(R.id.cityName);
                 RestConnection rest = new RestConnection();
                 String restResult = null;
@@ -51,28 +44,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public JsonObject transformStringToJson(String jsonString) {
         JsonObject obj = new JsonParser().parse(jsonString).getAsJsonObject();
         return obj;
@@ -82,15 +53,25 @@ public class MainActivity extends AppCompatActivity {
         JsonElement element = obj.get("main");
         JsonObject tmp = element.getAsJsonObject();
         String temperature = tmp.get("temp").toString();
-        float tempInt = Float.parseFloat(temperature);
-        tempInt -= 272.15f;
 
-        temperature = "" + String.format("%.1f", tempInt) + " c";
+        float tempFloat = Float.parseFloat(temperature);
+        tempFloat -= 272.15f;
+
+        int tempInt = Math.round(tempFloat);
+
+        // Used only if temperature will be shown with one decimal.
+        //temperature = "" + String.format("%.1f", tempInt) + "°c";
+
+        temperature = "" + tempInt + "°C";
+
+
+
         return temperature;
     }
 
     public String getCityFromJson(JsonObject obj) {
         String city = obj.get("name").toString();
+        city = city.replaceAll("^\"|\"$", "");
 
         return city;
     }
