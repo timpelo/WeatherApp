d8f5284b8c2c2206e322d8b2e39e0af5b3203bd6
KoskiJ <janne.j.koskinen@eng.tamk.fi>
2016-03-16 09:43:16 +0200

    New branch

--- app/src/main/java/tamk/tiko/com/weatherapp/MainActivity.java
+++ app/src/main/java/tamk/tiko/com/weatherapp/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
