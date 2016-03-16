1b31ba9608ce63e05d64d668748eaedbbbfa27de
KoskiJ <janne.j.koskinen@eng.tamk.fi>
2016-03-16 19:10:15 +0200

    Added dynamic weather icon and clickable widget

--- app/src/main/java/tamk/tiko/com/weatherapp/WeatherWidget.java
+++ app/src/main/java/tamk/tiko/com/weatherapp/WeatherWidget.java
@@ -1,8 +1,12 @@
 package tamk.tiko.com.weatherapp;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
 
 /**
@@ -14,12 +18,58 @@ public class WeatherWidget extends AppWidgetProvider {
                                 int appWidgetId) {
 
         CharSequence widgetLocation = context.getString(R.string.city);
-        CharSequence widgetTemp = context.getString(R.string.celsius) +
-                "℃";
+        CharSequence widgetTemp = "";
+
+        if (context.getString(R.string.unit).equals("1")) {
+            widgetTemp = context.getString(R.string.celsius) + "℃";
+        } else if (context.getString(R.string.unit).equals("2")) {
+            widgetTemp = context.getString(R.string.fahrenheit) + "℉";
+        }
+
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.weather_widget);
         views.setTextViewText(R.id.locationView, widgetLocation);
         views.setTextViewText(R.id.tempView, widgetTemp);
+        views.setImageViewResource(R.id.widgetIcon, R.drawable.icon_snow);
+
+        int id = 900;
+
+        try {
+            id = Integer.parseInt(context.getString(R.string.condition_id));
+        } catch (Exception e) {
+            Log.w("updateAppWidget()", "can't parse condition");
+        }
+
+        if(id >= 200 && id <= 232) {
+            views.setImageViewResource(R.id.widgetIcon, R.drawable.icon_thunder);
+        }
+        else if(id >= 300 && id <= 321) {
+            views.setImageViewResource(R.id.widgetIcon, R.drawable.icon_drizzle);
+        }
+        else if(id >= 500 && id <= 531) {
+            views.setImageViewResource(R.id.widgetIcon, R.drawable.icon_rain);
+        }
+        else if(id >= 600 && id <= 622) {
+            views.setImageViewResource(R.id.widgetIcon, R.drawable.icon_snow);
+        }
+        else if(id >= 701 && id <= 761) {
+            views.setImageViewResource(R.id.widgetIcon, R.drawable.icon_mist);
+        }
+        else if(id == 800) {
+            views.setImageViewResource(R.id.widgetIcon, R.drawable.icon_sunny);
+        }
+        else if(id >= 801 && id <= 804) {
+            views.setImageViewResource(R.id.widgetIcon, R.drawable.icon_cloudy);
+        }
+        else{
+            views.setImageViewResource(R.id.widgetIcon, R.drawable.icon_na);
+        }
+
+        Intent defineIntent = new Intent(context, MainActivity.class);
+        //defineIntent.setClassName(context,"MainActivity");
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,
+                0 /* no requestCode */, defineIntent, 0 /* no flags */);
+        views.setOnClickPendingIntent(R.id.widgetLayout, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
